7e3de7a945d2e51fc3b55b91f83a6821df97f5da
Zhiqiang He <rhiswell@gmail.com>
2019-06-01 15:11:59 +0800

    Update

--- config.c
+++ config.c
@@ -7,11 +7,10 @@
 #include "debug.h"
 #include "config.h"
 
-struct ConfigInfo config_info;
+struct config_info g_config_info;
 
 /* remove space, tab and line return from the line */
-void clean_up_line (char *line)
-{
+void clean_up_line(char *line) {
     char *i = line;
     char *j = line;
 
@@ -33,9 +32,8 @@ void clean_up_line (char *line)
  *       the number of nodes in the list
  */
 
-int parse_node_list (char *line, char ***node_list)
-{
-    int start = 0, end = 0, num_nodes=0;
+int parse_node_list(char *line, char ***node_list) {
+    int start = 0, end = 0, num_nodes = 0;
     char *i = line;
     char node_name_prefix[128] = {'\0'};
     char *j = node_name_prefix;
@@ -59,25 +57,24 @@ int parse_node_list (char *line, char ***node_list)
     }
 
     num_nodes = end - start + 1;
-    check (num_nodes > 0, "Invaild number of nodes: %d", num_nodes);
+    check(num_nodes > 0, "Invaild number of nodes: %d", num_nodes);
 
-    *node_list = (char **) calloc (num_nodes, sizeof(char *));
-    if (*node_list == NULL){
-        printf ("Failed to allocate node_list.\n");
+    *node_list = (char **)calloc(num_nodes, sizeof(char *));
+    if (*node_list == NULL) {
+        printf("Failed to allocate node_list.\n");
         return 0;
     }
 
     int k = 0, node_ind = start;
-    
+
     for (k = 0; k < num_nodes; k++) {
-        (*node_list)[k] = (char *) calloc (128, sizeof(char));
-        check ((*node_list)[k] != NULL,
-               "Failed to allocate node_list[%d]", k);
+        (*node_list)[k] = (char *)calloc(128, sizeof(char));
+        check((*node_list)[k] != NULL, "Failed to allocate node_list[%d]", k);
 
         if (strstr(node_name_prefix, "mnemosyne")) {
-            sprintf ((*node_list)[k], "mnemosyne%02d", node_ind);
+            sprintf((*node_list)[k], "mnemosyne%02d", node_ind);
         } else {
-            sprintf ((*node_list)[k], "saguaro%d", node_ind);
+            sprintf((*node_list)[k], "saguaro%d", node_ind);
         }
 
         node_ind += 1;
@@ -85,62 +82,61 @@ int parse_node_list (char *line, char ***node_list)
 
     return num_nodes;
 
- error:
+error:
     return -1;
 }
 
-int get_rank ()
-{
-    int			ret	    = 0;
-    uint32_t		i	    = 0;
-    uint32_t		num_servers = config_info.num_servers;
-    uint32_t		num_clients = config_info.num_clients;
-    struct utsname	utsname_buf;
-    char		hostname[64];
+int get_rank() {
+    int ret = 0;
+    uint32_t i = 0;
+    uint32_t num_servers = g_config_info.num_servers;
+    uint32_t num_clients = g_config_info.num_clients;
+    struct utsname utsname_buf;
+    char hostname[64];
 
     /* get hostname */
-    ret = uname (&utsname_buf);
-    check (ret == 0, "Failed to call uname");
+    ret = uname(&utsname_buf);
+    check(ret == 0, "Failed to call uname");
 
-    strncpy (hostname, utsname_buf.nodename, sizeof(hostname));
+    strncpy(hostname, utsname_buf.nodename, sizeof(hostname));
 
-    config_info.rank = -1;
+    g_config_info.rank = -1;
     for (i = 0; i < num_servers; i++) {
-        if (strstr(hostname, config_info.servers[i])) {
-            config_info.rank      = i;
-            config_info.is_server = true;
+        if (strstr(hostname, g_config_info.servers[i])) {
+            g_config_info.rank = i;
+            g_config_info.is_server = true;
             break;
         }
     }
 
     for (i = 0; i < num_clients; i++) {
-        if (strstr(hostname, config_info.clients[i])) {
-            if (config_info.rank == -1) {
-                config_info.rank      = i;
-                config_info.is_server = false;
+        if (strstr(hostname, g_config_info.clients[i])) {
+            if (g_config_info.rank == -1) {
+                g_config_info.rank = i;
+                g_config_info.is_server = false;
                 break;
             } else {
-                check (0, "node (%s) listed as both server and client", hostname);
+                check(0, "node (%s) listed as both server and client",
+                      hostname);
             }
         }
     }
 
-    check (config_info.rank >= 0, "Failed to get rank for node: %s", hostname);
+    check(g_config_info.rank >= 0, "Failed to get rank for node: %s", hostname);
 
     return 0;
- error:
+error:
     return -1;
 }
 
-int parse_config_file (char *fname)
-{
+int parse_config_file(char *fname) {
     int ret = 0;
     FILE *fp = NULL;
     char line[128] = {'\0'};
-    int  attr = 0;
+    int attr = 0;
 
-    fp = fopen (fname, "r");
-    check (fp != NULL, "Failed to open config file %s", fname);
+    fp = fopen(fname, "r");
+    check(fp != NULL, "Failed to open config file %s", fname);
 
     while (fgets(line, 128, fp) != NULL) {
         // skip comments
@@ -148,97 +144,94 @@ int parse_config_file (char *fname)
             continue;
         }
 
-        clean_up_line (line);
+        clean_up_line(line);
 
-	if (strstr (line, "servers:")) {
+        if (strstr(line, "servers:")) {
             attr = ATTR_SERVERS;
             continue;
-        } else if (strstr (line, "clients:")) {
+        } else if (strstr(line, "clients:")) {
             attr = ATTR_CLIENTS;
             continue;
-        } else if (strstr (line, "msg_size:")) {
+        } else if (strstr(line, "msg_size:")) {
             attr = ATTR_MSG_SIZE;
             continue;
-        } else if (strstr (line, "num_concurr_msgs:")) {
+        } else if (strstr(line, "num_concurr_msgs:")) {
             attr = ATTR_NUM_CONCURR_MSGS;
             continue;
         }
 
-	if (attr == ATTR_SERVERS) {
-            ret = parse_node_list (line, &config_info.servers);
-            check (ret > 0, "Failed to get server list");
-            config_info.num_servers = ret;
+        if (attr == ATTR_SERVERS) {
+            ret = parse_node_list(line, &g_config_info.servers);
+            check(ret > 0, "Failed to get server list");
+            g_config_info.num_servers = ret;
         } else if (attr == ATTR_CLIENTS) {
-            ret = parse_node_list (line, &config_info.clients);
-            check (ret > 0, "Failed to get client list");
-            config_info.num_clients = ret;
+            ret = parse_node_list(line, &g_config_info.clients);
+            check(ret > 0, "Failed to get client list");
+            g_config_info.num_clients = ret;
         } else if (attr == ATTR_MSG_SIZE) {
-            config_info.msg_size = atoi(line);
-            check (config_info.msg_size > 0,
-                   "Invalid Value: msg_size = %d",
-                   config_info.msg_size);
+            g_config_info.msg_size = atoi(line);
+            check(g_config_info.msg_size > 0, "Invalid Value: msg_size = %d",
+                  g_config_info.msg_size);
         } else if (attr == ATTR_NUM_CONCURR_MSGS) {
-            config_info.num_concurr_msgs = atoi(line);
-            check (config_info.num_concurr_msgs > 0,
-                   "Invalid Value: num_concurr_msgs = %d",
-                   config_info.num_concurr_msgs);
+            g_config_info.num_concurr_msgs = atoi(line);
+            check(g_config_info.num_concurr_msgs > 0,
+                  "Invalid Value: num_concurr_msgs = %d",
+                  g_config_info.num_concurr_msgs);
         }
 
         attr = 0;
     }
 
-    ret = get_rank ();
-    check (ret == 0, "Failed to get rank");
+    ret = get_rank();
+    check(ret == 0, "Failed to get rank");
 
-    fclose (fp);
+    fclose(fp);
 
     return 0;
 
- error:
+error:
     if (fp != NULL) {
-        fclose (fp);
+        fclose(fp);
     }
     return -1;
 }
 
-void destroy_config_info ()
-{
-    int num_servers = config_info.num_servers;
-    int num_clients = config_info.num_clients;
+void destroy_config_info() {
+    int num_servers = g_config_info.num_servers;
+    int num_clients = g_config_info.num_clients;
     int i;
 
-    if (config_info.servers != NULL) {
+    if (g_config_info.servers != NULL) {
         for (i = 0; i < num_servers; i++) {
-            if (config_info.servers[i] != NULL) {
-                free (config_info.servers[i]);
+            if (g_config_info.servers[i] != NULL) {
+                free(g_config_info.servers[i]);
             }
         }
-        free (config_info.servers);
+        free(g_config_info.servers);
     }
 
-    if (config_info.clients != NULL) {
+    if (g_config_info.clients != NULL) {
         for (i = 0; i < num_clients; i++) {
-            if (config_info.clients[i] != NULL) {
-                free (config_info.clients[i]);
+            if (g_config_info.clients[i] != NULL) {
+                free(g_config_info.clients[i]);
             }
         }
-        free (config_info.clients);
+        free(g_config_info.clients);
     }
 }
 
-void print_config_info ()
-{
-    log (LOG_SUB_HEADER, "Configuraion");
+void print_config_info() {
+    log(LOG_SUB_HEADER, "Configuraion");
 
-    if (config_info.is_server) {
-	log ("is_server                 = %s", "true");
+    if (g_config_info.is_server) {
+        log("is_server                 = %s", "true");
     } else {
-	log ("is_server                 = %s", "false");
+        log("is_server                 = %s", "false");
     }
-    log ("rank                      = %d", config_info.rank);
-    log ("msg_size                  = %d", config_info.msg_size);
-    log ("num_concurr_msgs          = %d", config_info.num_concurr_msgs);
-    log ("sock_port                 = %s", config_info.sock_port);
-    
-    log (LOG_SUB_HEADER, "End of Configuraion");
+    log("rank                      = %d", g_config_info.rank);
+    log("msg_size                  = %d", g_config_info.msg_size);
+    log("num_concurr_msgs          = %d", g_config_info.num_concurr_msgs);
+    log("sock_port                 = %s", g_config_info.sock_port);
+
+    log(LOG_SUB_HEADER, "End of Configuraion");
 }
